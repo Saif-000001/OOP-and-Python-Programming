c52febe540352911286464a01543264a1d77411c
MSI75 <sisaif56@gmail.com>
2024-01-10 00:06:03 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+""" print("WELLCOME TO MY NEW JURNY") """
+
+# print("HELLO WORLD")
+
+# num = ["ad", "dak", "ddaa"]
+
+num = [14,56,74,89]
+
+for index, value in enumerate(num):
+    print(f"index : {index} , value : {value} ")
