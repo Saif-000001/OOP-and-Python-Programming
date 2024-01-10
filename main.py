367ba656614939eaa0c345fe6e8152ba1ed8d5f1
MSI75 <sisaif56@gmail.com>
2024-01-10 11:56:45 +0600

    Fun

--- main.py
+++ main.py
@@ -4,7 +4,16 @@
 
 # num = ["ad", "dak", "ddaa"]
 
-num = [14,56,74,89]
+# num = [14,56,74,89]
 
-for index, value in enumerate(num):
-    print(f"index : {index} , value : {value} ")
+# for index, value in enumerate(num):
+    # print(f"index : {index} , value : {value} ")
+
+# def display_person(**kwargs):
+#     for key,value in kwargs.items():
+#         print(f"{key}: {value}")
+
+
+# display_person(Name="Amir Khan", Age="45")
+
+
